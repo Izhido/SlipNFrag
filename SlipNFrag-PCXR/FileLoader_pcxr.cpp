780e7ae936ddf0573738817c91761bb57ac60dd1
Heriberto Delgado <izhido@hotmail.com>
2025-09-21 00:05:55 -0600

    Added back network options to the PCXR version. Assets and shaders in the PCXR version will now be loaded from a single "slipnfrag_assets" folder accompanying the final executable.

--- SlipNFrag-PCXR/FileLoader_pcxr.cpp
+++ SlipNFrag-PCXR/FileLoader_pcxr.cpp
@@ -4,7 +4,8 @@
 
 void FileLoader_pcxr::Load(const char* filename, std::vector<unsigned char>& data)
 {
-    std::ifstream file(filename, std::ios::binary);
+    const std::string assets = "slipnfrag_assets/";
+    std::ifstream file(assets + filename, std::ios::binary);
     file.seekg(0, std::ios::end);
     auto length = file.tellg();
     file.seekg(0);
